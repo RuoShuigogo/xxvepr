5269a0258e3723945a309553a3936f82f4c97f75
bensenplus <12292394@qq.com>
2024-04-30 09:22:16 +0800

    更新集成步骤（最简示例）

---
+++ public/code/Simple.vue
@@ -0,0 +1,20 @@
+<template>
+     <el-button plain type="primary" @click="execCommand('print')">打印</el-button>
+     <Editor doc="https://www.x-emr.cn/doc/999.html" @load="onLoad" style="margin: 10px 0;" mode="design"></Editor>
+</template>
+
+<script setup>
+
+     var editor = null
+
+     //加载编辑器
+     const onLoad = (e) => {
+          editor =  e.target.contentWindow.editor
+     }
+
+     //处理文档命令
+     const execCommand = (cmd) => {
+          editor.execCommand(cmd)
+     }
+
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,12 @@
 <template>
     <h3>
-        Vue集成X-EMR编辑器
+        Vue 集成X-EMR 编辑器步骤
     </h3>
-    <h4>1.新建VUE组件 Editor.vue</h4>
+    <h4>1.新建组件 Editor.vue</h4>
     <Source src="/code/Editor.vue" open="true"></Source>
-    <h4>2.修改组件内嵌文件 editor.html</h4>
+    <h4>2.模块中使用@load获取组件的editor实例</h4>
+    <Source src="/code/Simple.vue" open="true"></Source>
+    <h4>3.自定义修改配置 editor.html</h4>
     <Source src="/editor.html" open="true"></Source>
 </template>
 
