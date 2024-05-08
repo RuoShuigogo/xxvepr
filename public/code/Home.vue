84534ba52378970f7894a845def576c80b7a6661
bensenplus <12292394@qq.com>
2024-05-08 18:48:17 +0800

    体温单：删除物理降温参数，放置到体温参数中以竖线分割

---
+++ public/code/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h3>
+        Vue 集成X-EMR 编辑器步骤
+    </h3>
+    <h4>1.新建组件 Editor.vue</h4>
+    <Source src="/code/Editor.vue" open="true"></Source>
+    <h4>2.模块中使用@load获取组件的editor实例</h4>
+    <Source src="/code/Simple.vue" open="true"></Source>
+    <h4>3.自定义修改配置 editor.html</h4>
+    <Source src="/editor.html" open="true"></Source>
+</template>
+
+<script setup>
+
+</script>
